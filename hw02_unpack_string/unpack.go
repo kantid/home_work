a083a2edbffc4e2222939847018307928c4f7ef6
a.krasnova.kantid <a.krasnova@propellerads.net>
2021-01-04 19:08:55 +0300

    hw02_linter_changes

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -1,4 +1,4 @@
-package hw02_unpack_string
+package hw02UnpackString
 
 import (
 	"errors"
@@ -6,32 +6,32 @@ import (
 	"unicode"
 )
 
-// ErrInvalidString - invalid string error
+// ErrInvalidString - invalid string error.
 var ErrInvalidString = errors.New("invalid string")
 
-// Unpack - unpack string
-// - s is entire string
-// - this func returns string results of Unpack and error, if it's throw
+// Unpack - unpack string;
+// - s is entire string;
+// - this func returns string results of Unpack and error, if it's throw.
 func Unpack(s string) (string, error) {
-	// res - results of Unpack function return, we use Builder to min memory usage
+	// res - results of Unpack function return, we use Builder to min memory usage.
 	var res strings.Builder
-	// lastRune -  last symbol before current
+	// lastRune -  last symbol before current.
 	var lastRune rune
 	var esc bool
 
-	//go throw string by every rune, i is current symbol position
+	// go throw string by every rune, i is current symbol position.
 	for i, curRune := range s {
-		//if first symbol is dijit, error occurred
+		//if first symbol is dijit, error occurred.
 		if unicode.IsDigit(curRune) && i == 0 {
 			return "", ErrInvalidString
 		}
 
-		//if we get number instead of digit, error occurred
+		// if we get number instead of digit, error occurred.
 		if unicode.IsDigit(curRune) && unicode.IsDigit(lastRune) {
 			return "", ErrInvalidString
 		}
 
-		//if current rune is letter or space character, write current rune to res
+		// if current rune is letter or space character, write current rune to res.
 		if unicode.IsLetter(curRune) || unicode.IsSpace(curRune) || unicode.IsSymbol(curRune) {
 			res.WriteRune(curRune)
 		}
@@ -40,16 +40,16 @@ func Unpack(s string) (string, error) {
 			res.WriteRune(curRune)
 		}
 
-		//if current symbol is digit & there's letter after, or last symbol = space character
+		// if current symbol is digit & there's letter after, or last symbol = space character.
 		if (unicode.IsDigit(curRune) && !esc) && (unicode.IsLetter(lastRune) || unicode.IsSpace(lastRune) || unicode.IsSymbol(lastRune)) {
-			//convert rune to int
+			// convert rune to int.
 			runeInt := int(curRune - '0')
-				//if current rune is '0', remove lastRune from result
+				// if current rune is '0', remove lastRune from result.
 				if runeInt == 0 {
 					s = strings.TrimSuffix(res.String(), string(lastRune))
 					res.Reset()
 					res.WriteString(s)
-					//if not 0, repeat last rune by current rune value
+					//if not 0, repeat last rune by current rune value.
 				} else {
 					res.WriteString(strings.Repeat(string(lastRune), runeInt-1))
 			}
@@ -63,11 +63,11 @@ func Unpack(s string) (string, error) {
 
 		if esc {}
 
-		// set lastRune to current rune
+		// set lastRune to current rune.
 		lastRune = curRune
 	}
 
-	//return result, nil because we don't have error here
+	// return result, nil because we don't have error here.
 	return res.String(), nil
 
 }
