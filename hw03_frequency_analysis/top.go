e10f0db8798f06c114482e94b45f073c456faaa4
a.krasnova.kantid <a.krasnova@propellerads.net>
2021-01-04 22:30:23 +0300

    hw03_frequency_analysis

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,40 @@
-package hw03_frequency_analysis //nolint:golint,stylecheck
+package frequency
 
-func Top10(_ string) []string {
-	// Place your code here
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+type Pair struct {
+	Key   string
+	Value int
+}
+
+type PairList []Pair
+
+func Top10(inputStr string) []string {
+	if len(inputStr) == 0 {
+		return []string{}
+	}
+
+	wordsSequence := strings.Fields(inputStr)
+
+	wordFrequencies := make(map[string]int)
+	for i := range wordsSequence {
+		wordFrequencies[wordsSequence[i]]++
+	}
+
+	wordCount := make(PairList, len(wordFrequencies))
+	for key, value := range wordFrequencies {
+		wordCount = append(wordCount, Pair{key, value})
+	}
+
+	sort.Slice(wordCount, func(i, j int) bool { return wordCount[i].Value > wordCount[j].Value })
+
+	var s []string
+	for i := 0; i < 10; i++ {
+		s = append(s, wordCount[i].Key)
+	}
+
+	return s
 }
--- hw03_frequency_analysis/top_test.go
+++ hw03_frequency_analysis/top_test.go
@@ -1,4 +1,4 @@
-package hw03_frequency_analysis //nolint:golint
+package frequency //nolint:golint
 
 import (
 	"testing"
